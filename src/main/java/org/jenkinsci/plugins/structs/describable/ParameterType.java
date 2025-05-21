135f3d4cfa359275bbf338795004511e1d9788da
Zbynek Konecny <zbynek1729@gmail.com>
2025-05-21 21:57:36 +0200

    Increase log level

--- src/main/java/org/jenkinsci/plugins/structs/describable/ParameterType.java
+++ src/main/java/org/jenkinsci/plugins/structs/describable/ParameterType.java
@@ -82,15 +82,19 @@ public abstract class ParameterType {
                         if (entry.getValue().size() == 1) { // normal case: unambiguous via simple name
                             try {
                                 types.put(entry.getKey(), DescribableModel.of(entry.getValue().get(0)));
-                            } catch (Exception | NoClassDefFoundError x) {
+                            } catch (Exception x) {
                                 LOGGER.log(Level.FINE, "skipping subtype", x);
+                            } catch (NoClassDefFoundError x) {
+                                LOGGER.log(Level.WARN, "skipping subtype", x);
                             }
                         } else { // have to diambiguate via FQN
                             for (Class<?> subtype : entry.getValue()) {
                                 try {
                                     types.put(subtype.getName(), DescribableModel.of(subtype));
-                                } catch (Exception | NoClassDefFoundError x) {
+                                } catch (Exception x) {
                                     LOGGER.log(Level.FINE, "skipping subtype", x);
+                                } catch (NoClassDefFoundError x) {
+                                    LOGGER.log(Level.WARN, "skipping subtype", x);
                                 }
                             }
                         }
